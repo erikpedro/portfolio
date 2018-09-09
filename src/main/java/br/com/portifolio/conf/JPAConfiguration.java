f7b6e4366b89037a8a701477be4911de163d4e55
Erik Pedro <erikpedro@gmail.com>
2018-09-09 00:13:10 -0300

    Confi postgresql

--- src/main/java/br/com/portifolio/conf/JPAConfiguration.java
+++ src/main/java/br/com/portifolio/conf/JPAConfiguration.java
@@ -3,8 +3,10 @@ package br.com.portifolio.conf;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -16,38 +18,47 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JPAConfiguration {
 	
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManageFactory() {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, 
+				Properties additionalProperties) {
+		
 		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
-		
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUsername("root");
-		dataSource.setPassword("Java@Spring123");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/portifolio");
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		
+
+		factoryBean.setPackagesToScan("br.com.portifolio.models");
 		
 		factoryBean.setDataSource(dataSource);
-		
-		Properties props = new Properties();
-		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		props.setProperty("hibernate.show_sql", "true");
-		props.setProperty("hibernate.hbm2ddl.auto", "update");
-		
-		factoryBean.setJpaProperties(props);
-		
-		factoryBean.setPackagesToScan("br.com.portifolio.models");
+		factoryBean.setJpaProperties(additionalProperties);
 		
 		return factoryBean;
 	}
-	
+
+	@Bean
+	@Profile("dev")
+	public Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		return properties;
+	}
+
+	@Bean
+	@Profile("dev")
+	private DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("root");
+		dataSource.setPassword("Java@Spring123");
+		dataSource.setUrl("jdbc:mysql://localhost/portifolio");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		return dataSource;
+	}
+
 	@Bean
 	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
 		return new JpaTransactionManager(emf);
 	}
-	
 	
 
 }
--- src/main/java/br/com/portifolio/conf/ServletSpringMVC.java
+++ src/main/java/br/com/portifolio/conf/ServletSpringMVC.java
@@ -2,8 +2,11 @@ package br.com.portifolio.conf;
 
 import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -12,7 +15,7 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {SpringSecurityConfiguration.class, AppWebConfiguration.class, JPAConfiguration.class};
+		return new Class[] {SpringSecurityConfiguration.class, AppWebConfiguration.class, JPAConfiguration.class, JPAProductionConfiguration.class};
 	}
 
 	@Override
@@ -43,5 +46,14 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	    HiddenHttpMethodFilter httpMethodFilter = new HiddenHttpMethodFilter();
 	    return new Filter[]{characterEncodingFilter, httpMethodFilter};
 	}
+	
+	
+	
+//	@Override
+//	public void onStartup(ServletContext servletContext) throws ServletException {
+//		super.onStartup(servletContext);
+//		servletContext.addListener(RequestContextListener.class);
+//		servletContext.setInitParameter("spring.profiles.active", "dev");
+//	}
 	 
 }
